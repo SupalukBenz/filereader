95904ea9fe2cefaea110e477e10afd5f1c5f5547
SupalukBenz <supaluk.j@ku.th>
2018-02-25 11:00:03 +0700

    Add Javadoc

--- src/FileTest.java
+++ src/FileTest.java
@@ -1,2 +1,23 @@
+/**
+ * Test that running file for find the elapse time
+ * @author Supaluk Jaroensuk
+ */
 public class FileTest {
+
+    /**
+     * Call any task for testing
+     * @param fileName name of file
+     */
+    public void test(String fileName){
+        TaskTimer taskTimer = new TaskTimer();
+        Runnable runnable = new AppendStringTask(fileName);
+        taskTimer.measureElapsed(runnable);
+
+        Runnable runnable1 = new AppendStringBuilder(fileName);
+        taskTimer.measureElapsed(runnable1);
+
+        Runnable runnable2 = new AppendStringByBuffered(fileName);
+        taskTimer.measureElapsed(runnable2);
+
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,15 @@ public class Main {
     /**
      * Name of file for reading
      */
-    private final static String fileName = "src/Alice-in-Wonderland-in-Wonderland.txt";
+    private final static String fileName = "src/Alice-in-Wonderland.txt";
 
     /**
      * Starting program
      * @param args not used
      */
     public static void main(String[] args) {
-        TaskTimer taskTimer = new TaskTimer();
-        Runnable runnable = new AppendStringTask(fileName);
-        taskTimer.measureElapsed(runnable);
-
-        Runnable runnable1 = new AppendStringBuilder(fileName);
-        taskTimer.measureElapsed(runnable1);
-
-        Runnable runnable2 = new AppendStringByBuffered(fileName);
-        taskTimer.measureElapsed(runnable2);
+       FileTest fileTest = new FileTest();
+       fileTest.test(fileName);
     }
 
 }
